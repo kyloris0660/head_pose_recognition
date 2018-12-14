6c1fe69ac2aac9ba34223d5a9febfee3d9f22e38
kyloris0660 <1489821378@qq.com>
2018-12-14 22:30:52 +0800

    fix bugs

--- data_process.py
+++ data_process.py
@@ -9,22 +9,25 @@ from sklearn.model_selection import train_test_split
 
 
 def data2array():
-    data = os.listdir(LABEL_PATH)
+    data = os.listdir(DATA_PATH)
     vec = []
     vec_of_pix = np.zeros((2790, 288, 384, 3))
+    cnt = 0
     for i in range(len(data)):
-        if os.path.splitext(LABEL_PATH + data[i])[-1] != '.txt':
+        if os.path.splitext(DATA_PATH + data[i])[-1] != '.jpg':
             pass
         else:
-            with open (LABEL_PATH + data[i], 'r', encoding="latin-1") as f:
-                foo = f.read()
-                a = re.split(r'\n', foo)
-                a = a[-5:-1]
-                a = [int(i) for i in a]
-                vec.append(a)
-            photo = Image.open(DATA_PATH + data[i].strip('.txt') + '.jpg')
-            vec_of_pix[i] = img_to_array(photo)
+            a = re.findall(r'[-+]?\d+', data[i])[-2:]
+            a = [int(i) for i in a]
+            vec.append(a)
+            
+            photo = Image.open(DATA_PATH + data[i])
+            vec_of_pix[cnt] = img_to_array(photo)
+            cnt += 1
     vec = np.asarray(vec)
+
+    assert vec.shape[0] == vec_of_pix.shape[0] == 2790
+
     np.save('ground_truth.npy', vec)
     np.save('pix_data.npy', vec_of_pix)
 
@@ -36,6 +39,10 @@ def gen_train_test(split_ratio=0.7, random_state=42):
     assert X.shape[0] == y.shape[0]
 
     return train_test_split(X, y, test_size=(1 - split_ratio), random_state=random_state)
+
+
+if '__name__' == '__main__':
+    data2array()
 
 
 
